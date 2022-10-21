8a51a228742f45330baa563aac83bf20498c5ba6
nachiappan <naachiappan15@gmail.com>
2022-10-21 12:10:54 +0530

    footer added

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     <div className=' w-full h-14 lg:h-20  relative'>
         <div className='footerglassbg bg-black '/>
         <div className='h-full w-full flex gap-4 items-center justify-between   lg:px-20 text-xs lg:text-sm px-2'>
-            <h1 className='small_grad_text'>Copyright © 2022 <span className='hidden lg:blo'>Velammal Enginnering College</span><span className='lg:hidden'>VEC</span> "OZMENTA"</h1>
+            <h1 className='small_grad_text'>Copyright © 2022 <span className='hidden lg:block'>Velammal Enginnering College</span><span className='lg:hidden'>VEC</span> "OZMENTA"</h1>
             <h1 className='small_grad_text'>Developed By <span className='grad_text'>Nachiappan</span></h1>
 
             
